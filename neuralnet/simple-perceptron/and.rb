93f3eae8a9fe3e57ecb1ea2071f920d4a0b979a7
Shohei Aoki <shoaok@gmail.com>
2015-08-18 18:29:16 +0900

    しきい値の評価を等号を含むように変更
    動いた

--- neuralnet/simple-perceptron/and.rb
+++ neuralnet/simple-perceptron/and.rb
@@ -23,8 +23,8 @@ class AndPerceptron < Perceptron
   #[x1,x2,t]=[0,0,0],[0,1,0],[1,0,0],[1,1,1]
   def initialize
     @v0 = rand(0.5...1.0)
-    @v1 = rand(0...1.0)
-    @v2 = rand(0...1.0)
+    @v1 = rand(-0.5...0.5)
+    @v2 = rand(-0.5...0.5)
     puts "initial values: v1: #{@v1}, v2: #{@v2}"
   end
 
@@ -32,16 +32,16 @@ class AndPerceptron < Perceptron
       x1 = datum[0]
       x2 = datum[1]
       t = datum[2]
-      a1 = (@@w11*x1+@@w12*x2-@@w10) > 0 ? 1 : 0
-      a2 = (@@w21*x1+@@w22*x2-@@w20) > 0 ? 1 : 0
+      a1 = (@@w11*x1+@@w12*x2-@@w10) >= 0 ? 1 : 0
+      a2 = (@@w21*x1+@@w22*x2-@@w20) >= 0 ? 1 : 0
       y = (@v1*a1+@v2*a2-@v0) > 0 ? 1 : 0
       dv1 = @@epsilon*(t-y)*a1
       dv2 = @@epsilon*(t-y)*a2
       @v1 = @v1 + dv1 
       @v2 = @v2 + dv2 
       # if ((t-y) != 0)
-      #   puts "penalty: a1:#{a1} a2:#{a2}, t:#{t}, y:#{y}"
-      #   puts "trained #{index} times: dv1: #{dv1}, dv2: #{dv2}"
+        # puts "debug: x1:#{x1}, x2:#{x2}, a1:#{a1} a2:#{a2}, t:#{t}, y:#{y}"
+        # puts "trained #{index} times: dv1: #{dv1}, dv2: #{dv2}"
       # end
   end
 
@@ -49,7 +49,7 @@ class AndPerceptron < Perceptron
     # @train_datum = @@train_set.sample
     # single_train(@train_datum)
     @train_data = []
-    10000.times do
+    100.times do
       @train_data << @@train_set.sample
     end
 
@@ -69,9 +69,9 @@ class AndPerceptron < Perceptron
     x1 = datum[0]
     x2 = datum[1]
     t = datum[2]
-    a1 = (@@w11*x1+@@w12*x2-@@w10) > 0 ? 1 : 0
-    a2 = (@@w21*x1+@@w22*x2-@@w20) > 0 ? 1 : 0
-    y = (@v1*a1+@v2*a2-@v0) > 0 ? 1 : 0
+    a1 = (@@w11*x1+@@w12*x2-@@w10) >= 0 ? 1 : 0
+    a2 = (@@w21*x1+@@w22*x2-@@w20) >= 0 ? 1 : 0
+    y = (@v1*a1+@v2*a2-@v0) >= 0 ? 1 : 0
     puts "x1 = #{x1}, x2 = #{x2}, t = #{t}, y = #{y}"
   end
 
