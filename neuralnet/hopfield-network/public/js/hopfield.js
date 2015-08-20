1b563b369e7336cd1b27058f25344fa4c8396eb5
Shohei Aoki <shoaok@gmail.com>
2015-08-21 00:01:00 +0900

    color toggle implemented

--- neuralnet/hopfield-network/public/js/hopfield.js
+++ neuralnet/hopfield-network/public/js/hopfield.js
@@ -1,3 +1,15 @@
 $(function(){
 
+  $("td").on("click",function(){
+    var id = $(this).attr("id");
+    var num = id.split('-')[1];
+    if($(this).data("clicked") == false){
+      $(this).css("background-color","#99ff99");
+      $(this).data("clicked",true);
+    } else {
+      $(this).css("background-color","#ffffff");
+      $(this).data("clicked",false);
+    }
+  });
+
 });
